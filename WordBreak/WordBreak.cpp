0405dfd93ba99be27ce9bd8dc687a67cb17b4f38
ksbaghel <kulbhushan.baghel@gmail.com>
2017-11-06 16:35:04 +0530

    updated

--- WordBreak/WordBreak.cpp
+++ WordBreak/WordBreak.cpp
@@ -1,4 +1,4 @@
-// WordBreak.cpp : Defines the entry point for the console application.
+
 /*
 Given an input string and a dictionary of words, find out if the input string can be segmented into a space - separated 
 sequence of dictionary words.
@@ -13,7 +13,7 @@ The string can be segmented as "i like".
 
 Input:  ilikesamsung
 Output: Yes
-The string can be segmented as 
+The string can be segmented as //2338137 -   
 	"i like samsung"
 or  "i like sam sung".
 
@@ -30,7 +30,7 @@ using namespace std;
 
 bool isInDictionary(string A, vector<string> &dict)
 {
-	//check if string A is part ofthe dictionary
+	//check if string A is part of  the dictionary
 	auto it = dict.begin();
 	for (; it != dict.end(); ++it)
 	{
@@ -43,38 +43,39 @@ bool isInDictionary(string A, vector<string> &dict)
 	return false;
 }
 
-void wordBreak_Util(string A, int n, vector<string> &B, string curr_sol, vector<string> &sol_set)
+void wordBreak_Util(string s, int s_size, vector<string> &dict, string curr_sol, vector<string> &sol_set)
 {
-	for (int i = 1; i <= n; i++)
+	for (int i = 1; i <= s_size; i++)
 	{
-		string prefix = A.substr(0, i);
+		string prefix = s.substr(0, i);
 
-		if (isInDictionary(prefix, B))
+		if (isInDictionary(prefix, dict))
 		{
-			if (i == n)
+			if (i == s_size)
 			{
 				curr_sol += prefix;
 				sol_set.push_back(curr_sol);
 				return;
 			}
-			wordBreak_Util(A.substr(i, n - i), n - i, B, curr_sol + prefix + " ", sol_set);
+			wordBreak_Util(s.substr(i, s_size - i), s_size - i, dict, curr_sol + prefix + " ", sol_set);
 		}
 	}
 }
 
-vector<string> wordBreak_Recursive(string A, vector<string> &B) 
+vector<string> wordBreak_Recursive(string A, vector<string> &dict) 
 {
 	vector<string> sol_set;
 	string curr_sol = "";
-	wordBreak_Util(A, A.size(), B, curr_sol, sol_set);
+	wordBreak_Util(A, A.size(), dict, curr_sol, sol_set);
 
 	return sol_set;
 }
 
-vector<string> wordBreak_Iterative(string A, vector<string> &B)
+vector<string> wordBreak_Iterative(string A, vector<string> &wordList)
 {
 	//insert the dictionay words in to unordered_set
-	unordered_set<string> dict(B.begin(), B.end());
+	unordered_set<string> dict(wordList.begin(), wordList.end());
+
 	//Below words contains all the valid parse starting from the idx i of string A.
 	vector<vector<string> > words(A.length() + 1, vector<string>(0));
 	//initializing the last 
@@ -133,6 +134,7 @@ int main()
 	cout << endl;
 
 	cout << "------------------------wordBreak_Iterative---------------------------" << endl;
+	res.clear();
 	res = wordBreak_Iterative(A, B);
 	it = res.begin();
 	for (; it != res.end(); ++it)
